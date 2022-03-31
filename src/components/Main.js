411c7a0fdf307e94b1bb17698dedf88d31d8470c
Pilarski <kordian.pilarski@capgemini.com>
2022-03-31 12:29:31 +0200

    setup redux and add dummy post

--- src/components/Main.js
+++ src/components/Main.js
@@ -27,6 +27,65 @@ const Main = () => {
           </button>
         </div>
       </ShareBox>
+      <div>
+        <Article>
+          <SharedActor>
+            <a>
+              <img src="./images/user.svg" alt="user image" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+            <button>
+              <img src="./images/ellipsis.svg" alt="elipsis image" />
+            </button>
+          </SharedActor>
+          <Description>desc</Description>
+          <SharedImg>
+            <a>
+              <img src="./images/shared-img.jpg" alt="shared image" />
+            </a>
+          </SharedImg>
+          <SocialCounts>
+            <li>
+              <button>
+                <img
+                  src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb"
+                  alt="like"
+                />
+                <img
+                  src="https://static-exp1.licdn.com/sc/h/5thsbmikm6a8uov24ygwd914f"
+                  alt="hand clap "
+                />
+                <span>7</span>
+              </button>
+            </li>
+            <li>
+              <a>4 comments</a>
+            </li>
+          </SocialCounts>
+          <SocialActions>
+            <button>
+              <img src="./images/like.svg" alt="like button " />
+              <span>Like</span>
+            </button>
+            <button>
+              <img src="./images/comment.svg" alt="comment button " />
+              <span>Comments</span>
+            </button>
+            <button>
+              <img src="./images/share.svg" alt="share button" />
+              <span>Share</span>
+            </button>
+            <button>
+              <img src="./images/send.svg" alt="send button" />
+              <span>Send</span>
+            </button>
+          </SocialActions>
+        </Article>
+      </div>
     </Container>
   );
 };
@@ -101,6 +160,135 @@ const ShareBox = styled(CommonCard)`
         span {
           color: #70b5f9;
         }
+      }
+    }
+  }
+`;
+
+const Article = styled(CommonCard)`
+  padding: 0;
+  margin: 0 8px;
+  overflow: visible;
+`;
+
+const SharedActor = styled.div`
+  padding-right: 40px;
+  flex-wrap: nowrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+    img {
+      width: 48px;
+      height: 48px;
+    }
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+    img {
+      width: 22px;
+    }
+  }
+`;
+
+const Description = styled.div`
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
+`;
+const SharedImg = styled.div`
+  margin-top: 8px;
+  wisth: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const SocialCounts = styled.ul`
+  list-style: none;
+  line-height: 1.3;
+  display: flex;
+  align-items: flex-start;
+  overflow: auto;
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px solid #e9e5df;
+  li {
+    margin-right: 5px;
+    font-size: 12px;
+    button {
+      display: flex;
+    }
+  }
+`;
+
+const SocialActions = styled.div`
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin: 0;
+  min-height: 40px;
+  padding: 4px 8px;
+
+  img {
+    max-height: 18px;
+  }
+
+  button {
+    display: inline-flex;
+    align-items: center;
+    padding: 4px;
+    color: #0a66c2;
+    min-width: 90px;
+    span {
+      margin-left: 4px;
+    }
+
+    @media (min-width: 768px) {
+      span {
+        margin-left: 8px;
       }
     }
   }
